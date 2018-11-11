3bba8bc4bc2f75a218f8cdc0bfc36a63fe1d4b50
AlfredTH <natlukvic@gmail.com>
2018-11-11 19:17:20 +0100

    Update index.py

--- www/index.py
+++ www/index.py
@@ -1,5 +1,8 @@
+"""
+Webpage example : a python script implementing the "request_handler" function
+"""
+
 def request_handler(request_details):
-	print(request_details.body)
 	get_parameters_dict = request_details.parsers.query(request_details.uri['query'])
 	prenom = get_parameters_dict['firstname'] if 'firstname' in get_parameters_dict else 'stranger'
 	body = """<html><body>
